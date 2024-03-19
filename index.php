86f6fcab521347452f9f358fba9d78ce50ec8c2f
RCLDevelopers <rasshtech@gmail.com>
2024-03-19 08:38:14 +0300

    First commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="https://codingbirdsonline.com/wp-content/uploads/2019/12/cropped-coding-birds-favicon-2-1-192x192.png" type="image/x-icon">
+    <link rel="icon" href="" type="image/x-icon">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <title>How to partially hide email address in PHP using AJAX - Coding Birds Online</title>
@@ -16,7 +16,7 @@
         <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
             <div class="card card-signin my-5">
                 <div class="card-body">
-                    <center><img width="50" src="https://codingbirdsonline.com/wp-content/uploads/2019/12/cropped-coding-birds-favicon-2-1-192x192.png"></center>
+                    <center><img width="50" src="https://zangticsdigital.com/storage/web-logo-rr-1.png"></center>
                     <h5 class="card-title text-center">Partially hide email address in php </h5>
                     <form id="myForm">
                         <div class="form-label-group">
